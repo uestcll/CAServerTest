2b872464ad30b4c02d55d93db774b150d64ca8df
wangforthinker <924701889@qq.com>
2013-10-05 10:39:43 +0800

    update

--- clientTest/ClientMainTest.cpp
+++ clientTest/ClientMainTest.cpp
@@ -1,11 +1,29 @@
 #include "CLCAClientPostNetMsgBySocket.h"
 #include "CLCAPostNetMsgTool.h"
+#include "CLCAGETPKMsgSerializer.h"
+#include "CLCASerializer.h"
+#include "CLCADeSerializer.h"
+#include "CLCAClientContext.h"
 
 #define  SERVER_IP "192.168.0.1"
 #define  SERVER_PORT 19999
 int main(int argc,char** argv)
 {
 	CLCAPostNetMsgTool* clPostTool = new CLCAClientPostNetMsgBySocket(AF_INET,(uint8_t*)SERVER_IP,SERVER_PORT);
+	char* name = "TestServer";
+	CLCAGETPKMessage* msg = new CLCAGETPKMessage(strlen(name)+1,(uint8_t*)name,1,0);
+	CLCASerializer* serializer = new CLCAGETPKMsgSerializer;
+	
+	serializer->SerializeHead(PK_FORSGET,1);
+	serializer->Serialize(msg);
+	serializer->SerializeLength();
+	clPostTool->PostNetMessage(serializer->getSerializeChar(),serializer->getLength());
+
+	delete msg;
+	delete serializer;
+
+	CLCAClientContext* context = (CLCAClientContext*)clPostTool->ReadFromNet();
+
 
 	delete clPostTool;
 }
